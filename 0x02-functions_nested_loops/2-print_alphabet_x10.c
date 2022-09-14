9913afde9513d0d69b9a66d94f8851db71573ca5
Khomotso012 <u18092005@tuks.co.za>
2022-09-13 17:23:50 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,20 +7,16 @@
 * prints 10 times the alphabet
 * Return: 0
 */
-int main(void)
+void print_alphabet_x10(void)
 {
-	void print_alphabet_x10(void)
-	{
-		char c, i;
+	char c, i;
 
-		for (i = 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
 		{
-			for (c = 'a'; c <= 'z'; c++)
-			{
-				_putchar(c);
-			}
-			_putchar('\n');
+			_putchar(c);
 		}
+		_putchar('\n');
 	}
-	return (0);
 }
