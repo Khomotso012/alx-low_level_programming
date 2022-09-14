e311f0240a5e74e40acc90bbb15a61366ae6cfe2
Khomotso012 <u18092005@tuks.co.za>
2022-09-13 17:07:43 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,12 +9,15 @@
 */
 void print_alphabet_x10(void)
 {
-	char c;
-	char i;
+	char c, i;
 
 	for (i = 0; i <= 9; i++)
 	{
-		_putchar(c);
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
+	return (0);
 }
