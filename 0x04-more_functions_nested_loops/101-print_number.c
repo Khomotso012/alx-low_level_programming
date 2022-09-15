096d542bbc39e0d9f8dfc89d9775c222327b5f7e
Khomotso012 <u18092005@tuks.co.za>
2022-09-15 16:07:33 -0700

    Task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_number - Prints a number
+* @n: parameter
+*/
+
+
+
+
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		putchar('-');
+		i = -i;
+	}
+	if (i > 9)
+	{
+		print_number(i / 10);
+	}
+	putchar(i % 10 + '0');
+}
