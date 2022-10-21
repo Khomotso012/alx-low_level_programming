c566ab82a5f095c199b369ee41043d3b3178f96c
Khomotso012 <u18092005@tuks.co.za>
2022-10-21 14:30:43 -0700

    101

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,25 @@
+;Comment: 64-bit program in assembly that prints Hello, Holberton, followed by a new line.
+; File: 101-hello_holberton.asm
+; Auth: Khomotso Mohlahlana
+
+extern printf
+
+section .text
+	global main
+
+main:
+	push rbp
+
+	mov rdi,fmt
+	mov rsi,msg
+	mov rax,0
+	call printf
+
+	pop rbp
+
+	mov rax,0
+	ret
+
+section .data
+	msg: db "Hello, Holberton", 0
+	fmt: db "%s", 10, 0
