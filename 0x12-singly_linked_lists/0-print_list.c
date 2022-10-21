7810908fc461747578fe84e47a26293c5330d48d
Khomotso012 <u18092005@tuks.co.za>
2022-10-21 12:46:53 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,7 +9,7 @@
 */
 size_t print_list(const list_t *h)
 {
-	size_t num_of_nodes = 0;
+	size_t num = 0;
 
 	while (h)
 	{
@@ -18,7 +18,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		num_of_nodes++;
+		num++;
 	}
-	return (num_of_nodes);
+	return (num);
 }
